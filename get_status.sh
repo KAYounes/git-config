eb41ecfe5a4f534050bbd8a641ccca02659310dc
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-11-30 11:22:45 +0200

    Finished

---
+++ get_status.sh
@@ -0,0 +1,64 @@
+# Echoes information about Git repository status when inside a Git repository
+git_info() {
+
+	# Exit if not inside a Git repository
+	! git rev-parse --is-inside-work-tree >/dev/null 2>&1 && return
+
+	# Git branch/tag, or name-rev if on detached head
+	local GIT_LOCATION=$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)
+	local GIT_CHANGES=$(git status --short | wc -l)
+
+	# Extract only the branch name
+	#   GIT_LOCATION=$(basename "$GIT_LOCATION")
+	if [ -z "$(git symbolic-ref -q HEAD)" ]; then
+		GIT_LOCATION="(detach HEAD)${GIT_LOCATION}"
+	else
+		# Extract only the branch name
+		GIT_LOCATION=$(basename "$GIT_LOCATION")
+	fi
+
+	local AHEAD="AHEAD"
+	local BEHIND="BEHIND"
+	local MERGING="MERGING"
+	local UNTRACKED="UNTRACKED"
+	local MODIFIED="MODIFIED"
+	local STAGED="STAGED"
+
+	local DIVERGENCES=""
+	local FLAGS=""
+
+	local NUM_AHEAD=$(git log --oneline @{u}.. 2>/dev/null | wc -l | tr -d ' ')
+	if [ "$NUM_AHEAD" -gt 0 ]; then
+		DIVERGENCES+=" ${AHEAD//NUM/$NUM_AHEAD}"
+	fi
+
+	local NUM_BEHIND=$(git log --oneline ..@{u} 2>/dev/null | wc -l | tr -d ' ')
+	if [ "$NUM_BEHIND" -gt 0 ]; then
+		DIVERGENCES+=" ${BEHIND//NUM/$NUM_BEHIND}"
+	fi
+
+	local GIT_DIR=$(git rev-parse --git-dir 2>/dev/null)
+	if [ -n "$GIT_DIR" ] && test -r "$GIT_DIR/MERGE_HEAD"; then
+		FLAGS+=" $MERGING"
+	fi
+
+	if [[ -n $(git ls-files --other --exclude-standard 2>/dev/null) ]]; then
+		FLAGS+=" $UNTRACKED"
+	fi
+
+	if ! git diff --quiet 2>/dev/null; then
+		FLAGS+=" $MODIFIED"
+	fi
+
+	if ! git diff --cached --quiet 2>/dev/null; then
+		FLAGS+=" $STAGED"
+	fi
+
+	echo "${GIT_LOCATION}${DIVERGENCES}${FLAGS}${GIT_CHANGES}" #±
+
+}
+
+# Use ❯ as the non-root prompt character; # for root
+# Change the prompt character if the last command had a nonzero exit code
+PS1='$(git_info) '
+
