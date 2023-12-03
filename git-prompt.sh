b548524b25877e49a2407b831f9397f0ee05687a
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-12-03 07:25:36 +0200

    Remove default code

--- git-prompt.sh
+++ git-prompt.sh
@@ -199,28 +199,28 @@ PS1="$PS1$green$I_DIRECTORY"' \[$(pwd | sed -E -e "s|^'"$HOME"'|~|" -e "s|^.*/([
 PS1="$PS1"'$(count_files)'
 PS1="$PS1\012"
 
-if test -z "$WINELOADERNOEXEC"; then
-	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
-	COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
-	COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-	if test -f "$COMPLETION_PATH/git-prompt.sh"; then
-		. "$COMPLETION_PATH/git-completion.bash"
-		. "$COMPLETION_PATH/git-prompt.sh"
+# if test -z "$WINELOADERNOEXEC"; then
+# 	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+# 	COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+# 	COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+# 	COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+# 	if test -f "$COMPLETION_PATH/git-prompt.sh"; then
+# 		. "$COMPLETION_PATH/git-completion.bash"
+# 		. "$COMPLETION_PATH/git-prompt.sh"
 		PS1="$PS1"'$(git_info)'
-	fi
-fi
+	# fi
+# fi
 
 PS1="$PS1$NO_FORMAT$C_DODGERBLUE1\012\$\[$NO_FORMAT\] "
 
 #######################
-MSYS2_PS1="$PS1" # for detection by MSYS2 SDK's bash.basrc
+# MSYS2_PS1="$PS1" # for detection by MSYS2 SDK's bash.basrc
 
-# Evaluate all user-specific Bash completion scripts (if any)
-if test -z "$WINELOADERNOEXEC"; then
-	for c in "$HOME"/bash_completion.d/*.bash; do
-		# Handle absence of any scripts (or the folder) gracefully
-		test ! -f "$c" ||
-			. "$c"
-	done
-fi
+# # Evaluate all user-specific Bash completion scripts (if any)
+# if test -z "$WINELOADERNOEXEC"; then
+# 	for c in "$HOME"/bash_completion.d/*.bash; do
+# 		# Handle absence of any scripts (or the folder) gracefully
+# 		test ! -f "$c" ||
+# 			. "$c"
+# 	done
+# fi
